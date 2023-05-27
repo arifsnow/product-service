24c6b3103ee3139569d7db8dd1e9a4d96161b100
Arif Saripudin <arifs.qh@gmail.com>
2023-05-28 03:15:45 +0700

    ubah tipe Id ke String

--- src/main/java/com/example/productservice/model/Product.java
+++ src/main/java/com/example/productservice/model/Product.java
@@ -1,9 +1,10 @@
 package com.example.productservice.model;
 
 import java.math.BigDecimal;
+import org.hibernate.annotations.GenericGenerator;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,8 +18,10 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Product {
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
+  @GeneratedValue(generator = "UUID")
+  @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+  @Column(updatable = false, nullable = false)
+  private String id;
   private String name;
   private String description;
   private BigDecimal price;
